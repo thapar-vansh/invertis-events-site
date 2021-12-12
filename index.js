08206f31e11622ffea5c1dfca396790eebc38f5b
Vansh <thaparvansh29@gmail.com>
2021-12-12 18:04:09 +0530

    Added database and event form

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
-import pg from 'pg'
-const { Pool } = pg
+import express from 'express'
+import bodyParser from 'body-parser'
+import conn from './connection.js'
+const app = express()
 
-const CONFIG = {
-  user: 'postgres',
-  password: 'root',
-  host: 'localhost',
-  port: 5432,
-  database: 'postgres',
-  max: 100,
-}
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }))
 
-const pool = new Pool(CONFIG)
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 
-export default {
-  query: (text, params, callback) => pool.query(text, params, callback)
-}
+app.post('/add', async (req, res) => {
+  let {
+    id,
+    name,
+    course,
+    sem,
+    contact_no,
+    email,
+    evt_name,
+    duration,
+    date,
+    conv_name,
+    descr,
+  } = req.body
+  conn.query(
+    'INSERT INTO eventdetails (id, name, course, sem, contact_no, email, evt_name, duration, date, conv_name, descr ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)',
+    [
+      id,
+      name,
+      course,
+      sem,
+      contact_no,
+      email,
+      evt_name,
+      duration,
+      date,
+      conv_name,
+      descr,
+    ],
+    (err, response) => {
+      if (err) {
+        console.log(err)
+        response = 'FAILED'
+      } else {
+        response = 'ADDED SUCCESSFULLY'
+        console.log(response)
+      }
+      res.send(response)
+      console.log(response)
+    }
+  )
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening at port 3000')
+})
